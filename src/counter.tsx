cf6aa6b63b13b2e0f3ace1762472bd8e00f7f908
abdumajid-501 <dasturchi501@gmail.com>
2026-01-28 07:24:04 +0500

    .

--- src/counter.tsx
+++ src/counter.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
-const Counter: React.FC = () => {
+const Counter = () => {
   const [count, setCount] = useState<number>(0);
 
   return (
